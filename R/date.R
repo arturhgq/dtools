3ebc2f090ac4788d2641e4161591061947d3de7c
Artur Quirino <arturhgq@mail.com>
2022-11-02 10:39:50 -0300

    adding methods in `as_decade()`

--- R/date.R
+++ R/date.R
@@ -3,10 +3,12 @@
 #'
 #' Returns a vector by grouping a string of years into decades
 #' @param year numeric variable of years
+#' @param method 'gregorian' or 'ISO'
 #' @examples
-#' as_decade(c(2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011))
+#' as_decade(c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011))
+#' as_decade(c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011), method = "ISO")
 #' @export
-as_decade = function(year) {
+as_decade = function(year, method = "gregorian") {
   if (!is.numeric(year)) {
     cli::cli_abort("'year' must be numeric.")
   }
@@ -14,6 +16,17 @@ as_decade = function(year) {
     if (!is.integer(x/10)) x - 1
     else x
   }
-  year_fix = sapply(year, subtract_one)
-  floor(year_fix/10)*10
+
+  if (method == "gregorian") {
+    year_fix = sapply(year, subtract_one)
+    floor(year_fix/10)*10
+
+  } else if (method == "ISO") {
+    floor(year/10)*10
+
+  } else {
+    cli::cli_abort("'method' must be 'gregorian' or 'ISO'.")
+  }
+
+
 }
