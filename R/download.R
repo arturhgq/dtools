51987d4d3765b74a5862509ef734923ebe5f4d0b
Artur Quirino <arturhgq@mail.com>
2023-02-01 21:52:31 -0300

    `download.files()` function

---
+++ R/download.R
@@ -0,0 +1,14 @@
+#' @title Download a set of files from the internet
+#' @description This function downloads a set of files from the internet.
+#' The result is a named list with the tempfile path of each downloaded file.
+#' @param x A character vector of urls.
+#' @export
+download.files = function(x) {
+  make = function(x) {
+    tempfile = tempfile()
+    download.file(x, tempfile, method = "curl")
+  }
+
+  files = sapply(x, make, simplify = F)
+  return(files)
+}
